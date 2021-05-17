9f5dd576ee0a7038e72007b1aeb0095c9cfdd78d
Ubuntu <ubuntu@ip-172-31-92-63.ec2.internal>
2021-05-17 21:20:54 +0000

    trying the new pipeline

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,2 +1,33 @@
-package io.pivotal.pal.tracker;public class EnvController {
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class EnvController {
+    private String port;
+    private String memLimit;
+    private String index;
+    private String addr;
+
+    public EnvController(@Value("${port:NOT SET}") String port,
+                         @Value("${memory.limit:NOT SET}") String memLimit,
+                         @Value("${cf.instance.index:NOT SET}") String index,
+                         @Value("${cf.instance.addr:NOT SET}") String addr)
+    {
+        this.port = port;
+        this.memLimit = memLimit;
+        this.index = index;
+        this.addr = addr;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv()
+    {
+        return Map.of("PORT", port, "MEMORY_LIMIT", memLimit,
+                      "CF_INSTANCE_INDEX", index, "CF_INSTANCE_ADDR", addr);
+    }
 }
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,2 +1,21 @@
-package io.pivotal.pal.tracker;public class WelcomeController {
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController
+{
+    private String helloMessage;
+
+    public WelcomeController(@Value("${welcome.message}") String helloMessage) {
+        this.helloMessage = helloMessage;
+    }
+
+    @GetMapping("/")
+    public String sayHello()
+    {
+        return this.helloMessage;
+    }
 }
